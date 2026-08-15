343ad665fb5361415b64064a042e10d480661cfa
Yetunde Adekoya <adecookie@Yetundes-MacBook-Neo.local>
2026-08-15 13:35:57 -0700

    This is my second commit

---
+++ classwork.py
@@ -0,0 +1,33 @@
+name = "Michael"
+age = 11 
+height = 1.52
+is_student = True
+favorite_food = "Pizza"
+score = 95
+temperature = 28.5
+colors = ["Red", "Blue", "Green"]
+numbers = [10, 20, 30]
+coordinates = (5, 8)
+days = ("Monday", "Tuesday", "Wednesday")
+student = {"name": "Michael", "age": 11}
+car = {"brand": "Toyota", "year": 2024}
+letters = {"A", "B", "C"}
+even_numbers = {2, 4, 6, 8}
+nothing = None
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(favorite_food))
+print(type(score))
+print(type(temperature))
+print(type(colors))
+print(type(numbers))
+print(type(coordinates))
+print(type(days))
+print(type(student))
+print(type(car))
+print(type(letters))
+print(type(even_numbers))
+print(type(nothing))
