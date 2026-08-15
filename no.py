00aebd586b50eaf08e2b0d3bfc27a9a8c2c33dae
Yetunde Adekoya <adecookie@Yetundes-MacBook-Neo.local>
2026-08-15 13:39:43 -0700

    This is my 6th commit

---
+++ no.py
@@ -0,0 +1,27 @@
+"""Question 1: Convert a String to an Integer The variable below stores a number as a string. age = "12" Convert age to an integer. Print the value. Print its data type. Question 2: Convert an Integer to a Float score = 95 Convert score to a float. Print the value. Print its data type. Question 3: Convert a Float to an Integer height = 1.75 Convert height to an integer. Print the value. Print its data type. What happened to the decimal part? Question 4: Convert an Integer to a String year = 2026 Convert year to a string. Print the value. Print its data type"""
+
+
+age = "12"
+print (type(age))
+age = int(age)
+
+print(age)
+print(type(age))
+
+score = 95
+score = float(score)
+
+print(score)
+print(type(score))
+
+height = 1.75
+height = int(height)
+
+print(height)
+print(type(height))
+
+year = 2026
+year = str(year)
+
+print(year)
+print(type(year))
